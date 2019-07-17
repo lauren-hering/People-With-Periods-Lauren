e5c97adfbef02b73a4004229bf0cab4ccbfb5d8b
lauren-hering <lauren.hering@comcast.net>
2019-07-17 13:25:17 -0500

    Initial Commit

---
+++ PeopleWithPeriods/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PeopleWithPeriods
+//
+//  Created by Apple on 7/17/19.
+//  Copyright © 2019 Lauren H, Abbey, Maeve. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
